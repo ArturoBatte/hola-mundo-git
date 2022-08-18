8f6516f092e55d8f5e4034c8befe03f4768f4d1b
ArturobBatte <arturobatte.00800@gmail.com>
2022-08-18 14:44:16 -0400

    Edad Juan

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let nombre =  "Juan Perez"
-let edad = 25;
+let edad = calcularEdad(2002);
 console.log("Hola" + nombre);
 console.log(nombre + "tiene" + edad + "años");
+
+functioncalcularEdad(anioNacimiento){
+        const fechaaActual = new Date();
+        const anioActual = fechaActual.fetFullyear
+        const edad = anioActual - anioNacimiento
+    } 
